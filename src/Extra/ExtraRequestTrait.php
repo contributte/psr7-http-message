59f5535e5bc4a9c3ae9385b67fef875d63ad100f
Milan Felix Šulc <rkfelix@gmail.com>
2018-04-24 22:29:08 +0200

    ExtraRequest: deprecate getBodyClone method, added new methods

--- src/Extra/ExtraRequestTrait.php
+++ src/Extra/ExtraRequestTrait.php
@@ -17,9 +17,12 @@ trait ExtraRequestTrait
 
 	/**
 	 * @return mixed
+	 * @deprecated
 	 */
 	public function getBodyClone()
 	{
+		trigger_error('This method is deprecated', E_DEPRECATED);
+
 		return clone $this->getBody();
 	}
 
@@ -32,12 +35,35 @@ trait ExtraRequestTrait
 	}
 
 	/**
+	 * @return mixed
+	 */
+	public function getContentsCopy()
+	{
+		$contents = $this->getContents();
+		$this->getBody()->rewind();
+
+		return $contents;
+	}
+
+	/**
 	 * @param bool $assoc
 	 * @return mixed
 	 */
 	public function getJsonBody($assoc = TRUE)
 	{
 		return json_decode((string) $this->getBody(), $assoc);
+	}
+
+	/**
+	 * @param bool $assoc
+	 * @return mixed
+	 */
+	public function getJsonBodyCopy($assoc = TRUE)
+	{
+		$contents = $this->getJsonBody($assoc);
+		$this->getBody()->rewind();
+
+		return $contents;
 	}
 
 	/**
