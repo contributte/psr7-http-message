3f2bcdcb3ccf67864a372dc11f31340adfb54d99
Marek Bartos <bartos.developer152@gmail.com>
2018-05-27 01:14:57 +0200

    Cleanup

--- tests/fixtures/JsonObject.php
+++ tests/fixtures/JsonObject.php
@@ -19,8 +19,7 @@ class JsonObject implements JsonSerializable
 	 * Specify data which should be serialized to JSON
 	 *
 	 * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-	 * @return mixed[] data which can be serialized by <b>json_encode</b>, which is a value of any type other than a
-	 *               resource.
+	 * @return mixed[] data which can be serialized by <b>json_encode</b>, which is a value of any type other than a resource.
 	 */
 	public function jsonSerialize(): array
 	{
